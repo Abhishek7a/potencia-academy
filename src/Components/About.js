b2578df6bbc4cfbf923e997ae4062c29037e3242
Your Name <you@example.com>
2023-06-24 18:10:19 +0530

    last and final

--- src/Components/About.js
+++ src/Components/About.js
@@ -31,7 +31,6 @@ export default function About() {
     }
 
     return (
-<<<<<<< HEAD
         <section className='mx-lg-3 rounded m-lg-5 m-md-5 about'>
             {/* <div   className='col-7 text-center bg-white rounded'> */}
             <h1 className='pt-5 text-center text-light fw-bold'>About Us</h1>
@@ -40,53 +39,8 @@ export default function About() {
                     <img src="https://www.potencia.in/wp-content/uploads/2023/03/Sir-1.webp" className='aboutImg mx-lg-5' alt="" />
                 </div>
                 <p className='mt-5 mx-lg-5 text-start fs-6  px-lg-5 px-md-1 mx-md-0 mx-sm-0 text-wrap text-light '>POTENCIA ACADEMY provides interactive lectures from qualified faculties to guide the students for their success in engineering and medical entrances and board examinations. Lays foundation from class IX for better future in any field. Also provides Aptitude classes for different competitive exams. All faculties are from IITs with good experience and the best knowledge in their respective subjects, who will lead you towards sucess by clearing basic concept without cramming them.</p>
-=======
-        <section className=' d-flex mx-5 mt-5  justify-content-between'>
-            <div className='about_section col-7 text-center'>
-                <h1 className='mt-2'>About Us</h1>
-                <p className='mt-5 mx-5'>POTENCIA ACADEMY provides interactive lectures from qualified faculties to guide the students for their success in engineering and medical entrances and board examinations. Lays foundation from class IX for better future in any field. Also provides Aptitude classes for different competitive exams. All faculties are from IITs with good experience and the best knowledge in their respective subjects, who will lead you towards sucess by clearing basic concept without cramming them.</p>
             </div>
-            <div className='form col-4'>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="exampleInputName" aria-describedby="emailHelp" required />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" required />
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Father Name</label>
-                        <input type="text" className="form-control" id="exampleInputFatherName" aria-describedby="emailHelp" required />
-                    </div>  <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Class</label>
-                        <input type="number" className="form-control" id="exampleInputClass" aria-describedby="emailHelp" required />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" autoComplete='off' className="form-label">Contact</label>
-                        <input type="number" className="form-control" id="exampleInputContact" aria-describedby="emailHelp" required />
-                    </div>
-                    {/* <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
-                    </div> */}
-                    {/* <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div> */}
-                    <button className="btn btn-primary" onClick={() => {
-                        setName(document.getElementById('exampleInputName').value);
-                        setEmail(document.getElementById('exampleInputEmail').value);
-                        setFatherName(document.getElementById('exampleInputFatherName').value);
-                        setClasss(document.getElementById('exampleInputClass').value);
-                        setContact(document.getElementById('exampleInputContact').value);
-                        handleSubmit();
-                    }}>Submit</button>
->>>>>>> e9319cf88bd63ffc604b16692ab0099a93299466
-            </div>
-            {/* <hr className='w-75 m-auto'></hr> */}
-
+    
         </section>
     )
 }
--- src/Components/Facilities.js
+++ src/Components/Facilities.js
@@ -2,40 +2,25 @@ import React from 'react'
 
 export default function Facilities() {
     return (
-<<<<<<< HEAD
-        < section className="bg-light">
+        <section className="bg-light">
             <h1 className='mt-2 text-center pt-5'>Facilities</h1>
             <div className='d-flex flex-wrap justify-content-around '>
-                <div className="card mb-3 mt-3  col-lg-3 col-md-5 col-sm-10 px-2"  style={{border :"1px solid #212844"}}>
+                <div className="card mb-3 mt-3  col-lg-3 col-md-5 col-sm-10 px-2" style={{ border: "1px solid #212844" }}>
                     <div className="row g-0">
                         <div className=" col-md-4 my-3 ml-2 text-center">
                             <img src="transport.jpg" className="img-fluid rounded-start " alt="transport" />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h5 className="card-title text-center" style={{color:"#212844"}}>Transport</h5>
-                                <p className="card-text" style={{color:"#212844"}}>The Potencia Academy operates a safe and affordable Transport facility for the Day Scholars.</p>
-=======
-        <>
-            <h1 className='mt-2 text-center mt-5'>Facilities</h1>
-            <div className='mx-5 d-flex  justify-content-between'>
-                <div className="card mb-3">
-                    <div className="row g-0 d-flex justify-content-around">
-                        <div className=" col-md-4 my-3 ">
-                            <img src="transport.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title text-center">Hostel</h5>
-                                <p className="card-text">The Potencia Academy has excellent accommodation for Girls and Boys. ~All students are provided with accommodation near Academy in well-appointed rooms in the hostels ~Twenty-four hours of Internet connectivity, fully automatic washing machines, and reading rooms ~Laundry and ironing services are provided.</p>
-                                {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
->>>>>>> e9319cf88bd63ffc604b16692ab0099a93299466
+                                <h5 className="card-title text-center" style={{ color: "#212844" }}>Transport</h5>
+                                <p className="card-text" style={{ color: "#212844" }}>The Potencia Academy operates a safe and affordable Transport facility for the Day Scholars.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <div className="card mb-3 mt-3  col-lg-3 col-md-5 col-sm-10 px-2" style={{backgroundColor:"#212844",color:"#fff"}}>
+                {/* </div> */}
+
+                <div className="card mb-3 mt-3  col-lg-3 col-md-5 col-sm-10 px-2" style={{ backgroundColor: "#212844", color: "#fff" }}>
                     <div className="row g-0 ">
                         <div className=" col-md-4 my-3 ml-2 text-center">
                             <img src="hostel.jpg" className="img-fluid rounded-start " alt="hostel" />
@@ -48,22 +33,22 @@ export default function Facilities() {
                         </div>
                     </div>
                 </div>
-                <div className="card mb-3 mt-3  col-lg-3 col-md-5 col-sm-10 px-2" style={{border :"1px solid #212844"}}>
+                <div className="card mb-3 mt-3  col-lg-3 col-md-5 col-sm-10 px-2" style={{ border: "1px solid #212844" }}>
                     <div className="row g-0 ">
                         <div className=" col-md-4 my-3 ml-2 text-center">
                             <img src="transport.jpg" className="img-fluid rounded-start " alt="hostel" />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h5 className="card-title text-center" style={{color:"#212844"}}>Regular Doubt Session</h5>
-                                <p className="card-text" style={{color:"#212844"}}>Individual attention to all the students separately in our booklet discussion and doubt solving sessions.</p>
+                                <h5 className="card-title text-center" style={{ color: "#212844" }}>Regular Doubt Session</h5>
+                                <p className="card-text" style={{ color: "#212844" }}>Individual attention to all the students separately in our booklet discussion and doubt solving sessions.</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* </div> */}
 
-                <div className="card mb-3  mt-3 col-lg-3 col-md-5 col-sm-10 mx-2 px-2" style={{backgroundColor:"#212844",color:"#fff"}}>
+                <div className="card mb-3  mt-3 col-lg-3 col-md-5 col-sm-10 mx-2 px-2" style={{ backgroundColor: "#212844", color: "#fff" }}>
                     <div className="row g-0">
                         <div className=" col-md-4 my-3 ml-2 text-center">
                             <img src="hostel.jpg" className="img-fluid rounded-start" alt="transport" />
@@ -76,20 +61,20 @@ export default function Facilities() {
                         </div>
                     </div>
                 </div>
-                <div className="card mb-3 col-lg-3 col-md-5  mt-3 mx-2 col-sm-10 px-2" style={{border :"1px solid #212844"}}>
+                <div className="card mb-3 col-lg-3 col-md-5  mt-3 mx-2 col-sm-10 px-2" style={{ border: "1px solid #212844" }}>
                     <div className="row g-0 ">
                         <div className=" col-md-4 my-3 ml-2 text-center ">
                             <img src="transport.jpg" className="img-fluid rounded-start" alt="hostel" />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h5 className="card-title text-center" style={{color:"#212844"}}>Competitive Environment</h5>
-                                <p className="card-text" style={{color:"#212844"}}>Formation of Diamond and Gold batches based on students performance and regular shuffling of students in these batches.</p>
+                                <h5 className="card-title text-center" style={{ color: "#212844" }}>Competitive Environment</h5>
+                                <p className="card-text" style={{ color: "#212844" }}>Formation of Diamond and Gold batches based on students performance and regular shuffling of students in these batches.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="card mb-3 col-md-5 col-lg-3 mt-3 mx-3 col-sm-10 px-2" style={{backgroundColor:"#212844",color:"#fff"}}>
+                <div className="card mb-3 col-md-5 col-lg-3 mt-3 mx-3 col-sm-10 px-2" style={{ backgroundColor: "#212844", color: "#fff" }}>
                     <div className="row g-0 ">
                         <div className=" col-md-4 my-3 ml-2 text-center">
                             <img src="hostel.jpg" className="img-fluid rounded-start" alt="hostel" />
@@ -100,36 +85,38 @@ export default function Facilities() {
                                 <p className="card-text">Motivational Booster for students to know areas which need more concentration and guards against mistakes in actual JEE/NEET.</p>
                             </div>
                         </div>
-=======
-                <div className="card " >
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*
+                    <div className="card " >
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card" >
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="card" >
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card" >
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="card" >
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card" >
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="card" >
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card" >
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
->>>>>>> e9319cf88bd63ffc604b16692ab0099a93299466
+                    <div className="card" >
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                        </div>
+                    </div> */}
                     </div>
                 </div>
             </div>
