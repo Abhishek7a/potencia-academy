f2ea7124e6dd85c11e0b4d556847a2be0f510307
Your Name <you@example.com>
2023-07-03 22:10:50 +0530

    repeat

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -25,7 +25,7 @@ export default function Form() {
                 const response = await fetch(API_URL);
                 const result = await response.json();
 
-        // console.log(result)
+        console.log(result)
 
         // emailjs.sendForm('service_uxv80ip', 'template_u19svt5', formm.current, 'XHYEBCkd47fUJJarO')
         //     .then((result) => {
