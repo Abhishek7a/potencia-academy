d985e4377b2ae9637c532e3b331ee272451166b5
Your Name <you@example.com>
2023-07-05 16:51:38 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,15 +26,15 @@ export default function Header() {
                     <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+                    {/* className={`${location.pathname === "/" ? "nav-link " : "nav-link"}`} */}
                     <div className="collapse navbar-collapse " id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 mx-2 fw-bold ">
+                            <Link onClick={goToTop} className="nav-link" style={{ color: "#212844", }} aria-current="page" to="/">  <li className="nav-item " data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Home </li></Link>
 
-                            <Link onClick={goToTop} className={`${location.pathname === "/" ? "link my-2" : "nav-link"}`} style={{ color: "#212844", }} aria-current="page" to="/">  <li className="nav-item mx-lg-2" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Home </li></Link>
-
-                            <Link onClick={goToTop} className={`${location.pathname === "/career" ? "link my-2 " : "nav-link"}`} aria-current="page" style={{ color: "#212844" }} to="/career" > <li className="nav-item mx-lg-2 " data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" > Career</li></Link>
+                            <Link onClick={goToTop} className="nav-link" aria-current="page" style={{ color: "#212844" }} to="/career" > <li className="nav-item " data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" > Career</li></Link>
 
                             <li className="nav-item dropdown mx-lg-2">
-                                <a className={`${location.pathname === "/results" ? "link my-2" : "nav-link"}`} role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "#212844" }}>
+                                <a className="nav-link" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "#212844" }}>
                                     Results<span className="mx-1" style={{ clipPath: "polygon(49% 74%, 0 39%, 100% 39%)", backgroundColor: "black" }}> ...</span>
                                 </a>
                                 <ul className="dropdown-menu ">
@@ -46,10 +46,10 @@ export default function Header() {
                             </li>
                             {/* dropdown-item */}
 
-                            <Link onClick={goToTop} className={`${location.pathname === "/aboutUs" ? "link my-2" : "nav-link"}`} style={{ color: "#212844" }} to="/aboutUs"> <li className="nav-item mx-lg-2" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">About Us </li></Link>
+                            <Link onClick={goToTop} className="nav-link"style={{ color: "#212844" }} to="/aboutUs"> <li className="nav-item mx-lg-2" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">About Us </li></Link>
 
                             <li className="nav-item dropdown mx-lg-2" >
-                                <a className={`${location.pathname === "/students" ? "link my-2" : "nav-link"}`} role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "#212844" }}>
+                                <a className="nav-link" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "#212844" }}>
                                     Students<span className="mx-1" style={{ clipPath: "polygon(49% 74%, 0 39%, 100% 39%)", backgroundColor: "black" }}> ...</span>
                                 </a>
                                 <ul className="dropdown-menu ">
@@ -60,10 +60,10 @@ export default function Header() {
                                 </ul>
                             </li>
 
-                            <Link onClick={goToTop} className={`${location.pathname === "/prex" ? "link my-2" : "nav-link"}`} to="/prex" style={{ color: "#212844" }}><li className="nav-item mx-lg-2" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Prex  </li></Link>
+                            <Link onClick={goToTop} className="nav-link" to="/prex" style={{ color: "#212844" }}><li className="nav-item mx-lg-2" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">Prex  </li></Link>
 
 
-                            <Link onClick={goToTop} className={`${location.pathname === "/contactUs" ? "link my-2" : "nav-link"}`} to="/contactUs" style={{ color: "#212844" }}><li className="nav-item mx-lg-2" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" >Contact Us</li></Link>
+                            <Link onClick={goToTop} className="nav-link" to="/contactUs" style={{ color: "#212844" }}><li className="nav-item mx-lg-2" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" >Contact Us</li></Link>
 
                         </ul>
 
