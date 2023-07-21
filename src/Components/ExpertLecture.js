55a726fa4227ada729362bb2054e512da8f1bfba
Your Name <you@example.com>
2023-07-22 02:42:37 +0530

    done from my side

--- src/Components/ExpertLecture.js
+++ src/Components/ExpertLecture.js
@@ -4,7 +4,7 @@ export default function ExpertLecture() {
     return (
         <>
             <h1 className='text-center mt-3'>Expert Video</h1>
-            <div className='d-flex justify-content-around my-2 mx-5 mt-md-0 mt-4 flex-wrap'>
+            <div className='d-flex justify-content-around my-2 mx-md-5 mt-md-0 mt-4 flex-wrap'>
                 <video width='50%' muted loop controls autoPlay className='mx-5 mt-sm-5 col-10 col-md-4 rounded' src="https://www.potencia.in/wp-content/uploads/2023/01/introvid.mp4"></video>
                 <video width='50%' muted loop controls autoPlay className='mx-5 mt-sm-5 mt-5 col-10 col-md-4 rounded' src="https://www.potencia.in/wp-content/uploads/2023/01/introvid.mp4"></video>
 
